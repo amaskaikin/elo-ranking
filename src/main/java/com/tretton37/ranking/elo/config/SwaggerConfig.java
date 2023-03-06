80c168fae66ec7c3271f2e24490fe31ad9efd1ad
Anton Maskaikin <anton.maskaikin@gmail.com>
2023-03-06 15:53:54 +0100

    Update Swagger OpenApi with Authentication

--- src/main/java/com/tretton37/ranking/elo/config/SwaggerConfig.java
+++ src/main/java/com/tretton37/ranking/elo/config/SwaggerConfig.java
@@ -1,7 +1,10 @@
 package com.tretton37.ranking.elo.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,12 +13,22 @@ import org.springframework.context.annotation.Configuration;
 public class SwaggerConfig {
     @Bean
     public OpenAPI openApiConfig() {
+        String securityScheme = "bearerAuth";
         return new OpenAPI()
                 .info(new Info()
                         .title("Elo Ranking Matchmaking management API")
                         .version("1.0.0")
                         .description("API for ranking games and playerIds")
-                );
+                )
+                .components(new Components()
+                        .addSecuritySchemes(securityScheme, new SecurityScheme()
+                                .name(securityScheme)
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")
+                        )
+                )
+                .addSecurityItem(new SecurityRequirement().addList(securityScheme));
     }
 
     @Bean
