0923ae5bdbacaa763d1b0768eacb54e7da21db8b
Anton Maskaikin <anton.maskaikin@gmail.com>
2023-03-06 15:40:05 +0100

    Secure API as Resource server

--- src/main/java/com/tretton37/ranking/elo/config/SecurityConfig.java
+++ src/main/java/com/tretton37/ranking/elo/config/SecurityConfig.java
@@ -1,10 +1,19 @@
 package com.tretton37.ranking.elo.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.JwtDecoders;
+import org.springframework.security.oauth2.jwt.JwtValidators;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -15,15 +24,25 @@ import java.util.List;
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Value("${auth0.audience}")
+    private String audience;
+
+    @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
+    private String issuer;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .authorizeHttpRequests()
-                .anyRequest()
-                .permitAll()
-                .and()
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(new AntPathRequestMatcher("/tournament/list/**"))
+                            .authenticated()
+                        .requestMatchers(new AntPathRequestMatcher("/**")).permitAll()
+                )
                 .csrf().disable()
                 .cors().configurationSource(corsConfig())
+                .and()
+                    .oauth2ResourceServer()
+                        .jwt(jwtConfigurer -> jwtConfigurer.decoder(jwtDecoder()))
                 .and()
                 .build();
     }
@@ -38,5 +57,21 @@ public class SecurityConfig {
         source.registerCorsConfiguration("/**", config);
 
         return source;
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        NimbusJwtDecoder jwtDecoder = JwtDecoders.fromOidcIssuerLocation(issuer);
+        jwtDecoder.setJwtValidator(
+                new DelegatingOAuth2TokenValidator<>(
+                        JwtValidators.createDefaultWithIssuer(issuer), tokenValidator())
+        );
+
+        return jwtDecoder;
+    }
+
+    @Bean
+    public OAuth2TokenValidator<Jwt> tokenValidator() {
+        return new SecurityAudienceValidator(audience);
     }
 }
