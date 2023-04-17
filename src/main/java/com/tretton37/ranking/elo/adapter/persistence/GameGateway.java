265d8c692cfae9b1af3d2de14c99cbb4b2fcf140
Anton Maskaikin <anton.maskaikin@gmail.com>
2023-04-17 10:37:33 +0200

    fix: Fix counting players games and ratings twice

--- src/main/java/com/tretton37/ranking/elo/adapter/persistence/GameGateway.java
+++ src/main/java/com/tretton37/ranking/elo/adapter/persistence/GameGateway.java
@@ -1,12 +1,11 @@
 package com.tretton37.ranking.elo.adapter.persistence;
 
+import com.tretton37.ranking.elo.adapter.mappers.PersistenceMapper;
+import com.tretton37.ranking.elo.application.persistence.entity.GameEntity;
+import com.tretton37.ranking.elo.application.persistence.repository.GameRepository;
 import com.tretton37.ranking.elo.application.persistence.specification.GameSpecificationBuilder;
 import com.tretton37.ranking.elo.domain.model.Game;
 import com.tretton37.ranking.elo.domain.model.search.GameSearchCriteria;
-import com.tretton37.ranking.elo.adapter.mappers.PersistenceMapper;
-import com.tretton37.ranking.elo.application.persistence.repository.GameRepository;
-import com.tretton37.ranking.elo.application.persistence.entity.GameEntity;
-import com.tretton37.ranking.elo.application.service.game.GameLifecycleManagerImpl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -23,15 +22,12 @@ public class GameGateway {
 
     private final GameRepository gameRepository;
     private final PersistenceMapper<Game, GameEntity> mapper;
-    private final GameLifecycleManagerImpl lifecycleManager;
 
     @Autowired
     public GameGateway(GameRepository gameRepository,
-                       PersistenceMapper<Game, GameEntity> mapper,
-                       GameLifecycleManagerImpl lifecycleManager) {
+                       PersistenceMapper<Game, GameEntity> mapper) {
         this.gameRepository = gameRepository;
         this.mapper = mapper;
-        this.lifecycleManager = lifecycleManager;
     }
 
     public Page<Game> getAll(Pageable pageable) {
@@ -51,8 +47,6 @@ public class GameGateway {
     }
 
     public Game save(Game game) {
-        lifecycleManager.register(game);
-
         return mapper.entityToDto(gameRepository.save(mapper.dtoToEntity(game)));
     }
 
