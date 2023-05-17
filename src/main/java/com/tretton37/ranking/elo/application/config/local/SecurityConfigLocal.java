f086a253bb4d4ddd32fd8376945683e3758e8367
Anton Maskaikin <anton.maskaikin@gmail.com>
2023-05-17 16:49:01 +0200

    Disable Security config for local profile

---
+++ src/main/java/com/tretton37/ranking/elo/application/config/local/SecurityConfigLocal.java
@@ -0,0 +1,27 @@
+package com.tretton37.ranking.elo.application.config.local;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@Profile("local")
+public class SecurityConfigLocal {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests()
+                .anyRequest().permitAll()
+                .and()
+                    .csrf()
+                        .disable()
+                    .cors()
+                        .disable()
+                .build();
+    }
+}
